57e7868e3311604a5b42d32c845ba89725e4819d
Paul-Greyrat <anhday1622005@gmail.com>
2025-11-30 12:07:22 +0700

    store hightlight tiles

--- Assets/_Scripts/Utils/PlacementProcess.cs
+++ Assets/_Scripts/Utils/PlacementProcess.cs
@@ -1,13 +1,14 @@
 
 
 
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlacementProcess
 {
     private GameObject m_PlacementOutLine;
     public BuildActionSO m_BuildAction;
-    public Vector3 m_HighlightPosition;
+    public Vector3Int[] m_HighlightPosition;
 
     public PlacementProcess(BuildActionSO buildAction)
     {
@@ -16,6 +17,11 @@ public class PlacementProcess
 
     public void Update()
     {
+        if (m_PlacementOutLine != null)
+        {
+            HighlightTiles(m_PlacementOutLine.transform.position);
+        }
+
         if (GreyUtils.TryGetHoldPosition(out Vector3 worldPosition))
         {
             m_PlacementOutLine.transform.position = SnapToGrid(worldPosition);
@@ -36,8 +42,23 @@ public class PlacementProcess
         return new Vector3(Mathf.Round(worldposition.x), Mathf.Round(worldposition.y), 0);
     }
 
-    void HighlightTiles()
+    void HighlightTiles(Vector3 outLinePosition)
     {
-        
+        Vector2Int buildingSize = new Vector2Int(2,3);
+        m_HighlightPosition = new Vector3Int[buildingSize.x * buildingSize.y];
+
+        for (int x = 0; x < buildingSize.x; x++)
+        {
+            for (int y = 0; y < buildingSize.y; y++)
+            {
+                m_HighlightPosition[x + y * buildingSize.x] = new Vector3Int((int)(outLinePosition.x + x), (int)(outLinePosition.y + y), 0);
+            }
+        }
+
+        foreach ( var tilePos in m_HighlightPosition)
+        {
+            Debug.Log(tilePos);
+        }
+        Debug.Log("----------------");
     }
 }
