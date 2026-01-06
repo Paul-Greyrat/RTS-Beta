c94d61920be8d4bed9c760fcec4a0376bf140e5f
Paul-Greyrat <anhday1622005@gmail.com>
2026-01-06 08:48:43 +0700

    feat(Grey): add text pobup

---
+++ Assets/_Scripts/Utils/TextPobup.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextPobup : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_Text;
+    
+}
